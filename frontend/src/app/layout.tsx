5c16e7873fa47399f2d51ee70c80fa0ee634ceb9
matt <55065240+mattbal@users.noreply.github.com>
2024-10-10 13:29:33 -0700

    updated favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import QueryProvider from '@/lib/QueryProvider';
 
 const inter = Inter({ weight: ['400'], subsets: ['latin'] });
 
@@ -17,9 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' type='image/png' href='/favicon-48x48.png' sizes='48x48' />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+      </head>
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
