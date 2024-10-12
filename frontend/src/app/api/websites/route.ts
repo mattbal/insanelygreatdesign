65ff10900cf3856e5b3cdddce41d928227c53c12
matt <55065240+mattbal@users.noreply.github.com>
2024-10-12 11:16:59 -0700

    added better error handling

--- frontend/src/app/api/websites/route.ts
+++ frontend/src/app/api/websites/route.ts
@@ -5,25 +5,31 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const cursor = searchParams.get('cursor');
 
-  if (!cursor || cursor === '0') {
-    const websites = await prisma.website.findMany({
-      take: 20,
-      orderBy: {
-        id: 'desc',
-      },
+  try {
+    if (!cursor || cursor === '0') {
+      const websites = await prisma.website.findMany({
+        take: 20,
+        orderBy: {
+          id: 'desc',
+        },
+      });
+      return Response.json(websites);
+    } else {
+      const websites = await prisma.website.findMany({
+        take: 20,
+        skip: 1, // skip the cursor
+        cursor: {
+          id: Number(cursor),
+        },
+        orderBy: {
+          id: 'desc',
+        },
+      });
+      return Response.json(websites);
+    }
+  } catch (error) {
+    return new Response(`${error}`, {
+      status: 500,
     });
-    return Response.json(websites);
-  } else {
-    const websites = await prisma.website.findMany({
-      take: 20,
-      skip: 1, // skip the cursor
-      cursor: {
-        id: Number(cursor),
-      },
-      orderBy: {
-        id: 'desc',
-      },
-    });
-    return Response.json(websites);
   }
 }
